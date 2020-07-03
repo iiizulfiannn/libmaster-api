4a9c5d797cd096847c393f18fe7e445ee1ce4116
Zulfian <iiizulfiannn@gmail.com>
2020-07-03 20:56:36 +0700

    update package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config({ path: "./.env" });
 
@@ -9,7 +10,10 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routeNavigator = require("./src/index");
 
-const server = app.listen(8080, "192.168.1.4", function () {
+// cors
+app.use(cors());
+
+const server = app.listen(8080, "127.0.0.1", function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log("Server Running at " + host + ": " + port);
@@ -22,20 +26,20 @@ app.use(express.static(__dirname + "/src/public/images/books"));
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "http://192.168.1.4:8081");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Accept, x-access-token, Authorization, Origin, X-Requested-With, Content-Type"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Expose-Headers", "Content-Length");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use(function (req, res, next) {
+//   //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Origin", "http://192.168.1.4:8081");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Accept, x-access-token, Authorization, Origin, X-Requested-With, Content-Type"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS"
+//   );
+//   res.header("Access-Control-Expose-Headers", "Content-Length");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use("/", routeNavigator);
